376430f9256442cf005b0f9e237e64ad12a7cf97
CasonRichardson <simpletheotter@gmail.com>
2025-02-12 22:26:21 -0500

    DONE LAB-2

--- lab5-2.cpp
+++ lab5-2.cpp
@@ -7,6 +7,27 @@ using namespace std;
 
 int main()
 {
+	const double DISC_RATE = 0.1;
+	const double SHIP_RATE1 = 0.99;
+	const double SHIP_RATE2 = 4.99;
+	double amountOwed = 0.0;
+	char member = ' ';
 
-	return 0;
+	cout << "Amount owed before any shipping and discount: ";
+	cin >> amountOwed;
+	cout << "Premier club member Y / N: ";
+	cin >> member;
+
+	if (toupper(member) == 'Y')
+		amountOwed == amountOwed * DISC_RATE;
+
+	if (amountOwed >= 100.0)
+		amountOwed += SHIP_RATE1;
+	else;
+		amountOwed += SHIP_RATE2;
+
+	cout << fixed << setprecision(2);
+	cout << "Amount owed after shipping and or discounts: " << amountOwed << endl;
+
+return 0;
 }	//end of main function
